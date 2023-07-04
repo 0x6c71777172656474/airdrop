3d2197fa826d489667ad6c5a5906a026f40f2978
0x6c71777172656474 <0xdenfisherman@gmail.com>
2023-07-04 18:43:17 +0700

    Here should be the Pausable feature, but ...

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 import {Roles} from "../lib/Roles.sol";
